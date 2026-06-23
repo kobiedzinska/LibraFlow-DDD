642876f27b17e37438ab5871b9e1b8e01f2f4732
kobiedzinska <obiedzinskakarolina2@gmail.com>
2026-06-23 21:27:04 +0200

    fixing payment adapter

--- Library/src/main/java/Payments/infrastructure/in/PaymentController.java
+++ Library/src/main/java/Payments/infrastructure/in/PaymentController.java
@@ -5,5 +5,7 @@ import Payments.application.ports.in.*;
 public class PaymentController {
 
 	private IPaymentUseCase finePaymentService;
-
+	public void processPayment(int paymentId){
+		finePaymentService.processPayment(paymentId);
+	}
 }
