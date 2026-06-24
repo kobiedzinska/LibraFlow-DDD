af25091b32c3e25b2cd335394956c1dc2ceb9554
Bartłomiej <krupaczek.b@gmail.com>
2026-06-24 15:51:33 +0200

    Cherry-picked main

--- Library/src/main/java/Main.java
+++ Library/src/main/java/Main.java
@@ -1,3 +1,14 @@
+import ReaderAccounts.application.ports.out.http.ICatalogEventPublisher;
+import ReaderAccounts.application.ports.out.http.ILoanPort;
+import ReaderAccounts.application.ports.out.http.IPaymentsPort;
+import ReaderAccounts.application.ports.out.persistence.IReaderRepository;
+import ReaderAccounts.application.service.ManageAccountsService;
+import ReaderAccounts.infrastructure.in.ManageAccountsControler;
+import ReaderAccounts.application.domain.service.*;
+import ReaderAccounts.infrastructure.out.http.CatalogEventPublisher;
+import ReaderAccounts.infrastructure.out.http.LoanPort;
+import ReaderAccounts.infrastructure.out.http.PaymentsPort;
+import ReaderAccounts.infrastructure.out.persistence.ReaderRepository;
 import Catalog.application.ports.in.ICatalogPort;
 import Catalog.application.ports.out.ICopyRepository;
 import Catalog.application.service.CopyStatusService;
@@ -10,8 +21,25 @@ import Loan.infrastructure.in.LoanController;
 import Loan.infrastructure.out.*;
 
 public class Main {
+    public static void main(String[] args) {        ICatalogEventPublisher domainEventPublisher = new CatalogEventPublisher();
+        IReaderRepository userRepository = new ReaderRepository();
+        ILoanPort loanPort = new LoanPort();
+        IPaymentsPort paymentsPort = new PaymentsPort();
 
-    public static void main(String[] args) {
+        CreateAccount createAccount = new CreateAccount();
+        DeleteAccount deleteAccount = new DeleteAccount();
+
+
+        // Tworzenie instancji ManageAccountsService
+        ManageAccountsService manageAccountsService = new ManageAccountsService(
+                domainEventPublisher, createAccount, deleteAccount, userRepository, loanPort, paymentsPort);
+
+        // Tworzenie instancji ManageAccountsControler z wykorzystaniem interfejsu ManageAccountsUseCase
+        ManageAccountsControler controller = new ManageAccountsControler(manageAccountsService);
+
+
+
+
         ILoanRepository loanRepository = new LoanRepository();
         IDomainEventPublisher domainEventPublisher = new DomainEventPublisher();
         LoanCopyPolicy loanCopyPolicy = new LoanCopyPolicy();
@@ -40,6 +68,4 @@ public class Main {
         loanController.returnLoan(1);
 
     }
-
-
 }
